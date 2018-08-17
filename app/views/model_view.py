5c1fe1d8fb5bf9dd211df45953fce62f4f52cd3f
Michael Kibuuka <awesomeme155@gmail.com>
2018-08-17 18:18:45 +0300

    [finishes #159853544] Add get_question_by_id view

--- app/views/model_view.py
+++ app/views/model_view.py
@@ -5,6 +5,7 @@ from app.models.model_manager import question_manager
 from app.response_helpers import response, response_for_creating_question
 from app.response_helpers import convert_list_to_json
 from app.response_helpers import response_for_get_all_questions
+from app.response_helpers import response_to_fetch_single_question
 
 
 qtn_bp = Blueprint('question', __name__, url_prefix='/api/v1')
@@ -48,9 +49,30 @@ class QuestionsView(MethodView):
             convert_list_to_json(questions), 200)
 
 
+class QuestionView(MethodView):
+
+    methods = ['GET']
+
+    # Get question by id
+    def get(self, qtn_id):
+        """
+        GET request to fetch a question by id
+        """
+        if not request.content_type == 'application/json':
+            return response('request must be of type json', 'failed', 400)
+
+        respo = question_manager.get_question(qtn_id)
+        if isinstance(respo, KeyError):
+            return response('Question ' + str(respo) + ' does not exist',
+                            'failed', 400)
+        return response_to_fetch_single_question(respo.jsonify(), 200)
+
+
 # Register a class as a view
 question_list = QuestionsView.as_view('questions')
+question = QuestionView.as_view('question')
 
 
 # Add url_rules for the API endpoints
 qtn_bp.add_url_rule('/questions', view_func=question_list)
+qtn_bp.add_url_rule('/questions/<int:qtn_id>', view_func=question)
