2a15d70bfc1a19ca43534b18db8e550927c6c492
Michael Kibuuka <awesomeme155@gmail.com>
2018-08-17 20:44:31 +0300

    Add response helper for jsonifying an answer

--- app/response_helpers.py
+++ app/response_helpers.py
@@ -34,6 +34,18 @@ def response_to_fetch_single_question(question, status_code):
     })), status_code
 
 
+def response_for_creating_an_answer(question, status_code):
+    return make_response(jsonify({
+        'status': 'success',
+        'id': question.id,
+        'title': question.title,
+        'body': question.body,
+        'tag': question.tag,
+        'date_posted': question.posted_date,
+        'answers': question.answers
+    })), status_code
+
+
 def convert_list_to_json(lsty):
     """
     converts a list to json
