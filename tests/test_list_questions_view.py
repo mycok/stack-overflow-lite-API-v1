2686b19222458cb38ea5418a8bf96eac41ec3e91
Michael Kibuuka <awesomeme155@gmail.com>
2018-08-17 17:31:40 +0300

    [finishes #159847938] Add tests for get_all_questions endpoint

--- tests/test_list_questions_view.py
+++ tests/test_list_questions_view.py
@@ -49,7 +49,43 @@ class TestCreateQuestion(BaseTestCase):
                 '/api/v1/questions',
                 content_type="xml",
                 data=json.dumps(dict(title='seriousness',
-                                body='seriousness is needed', tag='LIFE'))
+                                     body='seriousness is needed', tag='LIFE'))
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertTrue(data['message'], 'request must be of type json')
+            self.assertTrue(data['status'], 'failed')
+
+    # Test get all questions
+    def test_get_all_questions(self):
+        """
+        Test successful GET request to
+        fetch all questions
+        """
+        with self.client:
+            # create a question
+            post_response = self.create_question()
+            self.assertEqual(post_response.status_code, 201)
+
+            # get all questions
+            get_response = self.get_all_questions()
+
+            data = json.loads(get_response.data.decode())
+            self.assertEqual(get_response.status_code, 200)
+            self.assertIsInstance(data['questions'], list)
+            self.assertTrue(data['status'], 'success')
+            self.assertEqual(data['questions'][0]['id'], 1)
+            self.assertTrue(data['questions'][0]['title'], "seriousness")
+
+    def test_get_all_questions_with_wrong_content_type(self):
+        """
+        Test unsuccessful GET request to
+        fetch all questions with wrong content type
+        """
+        with self.client:
+            response = self.client.get(
+                '/api/v1/questions',
+                content_type='xml'
             )
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 400)
