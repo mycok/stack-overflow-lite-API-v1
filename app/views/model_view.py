f9df2b71b0fce1e58f39c907a268d0444f11e74f
Michael Kibuuka <awesomeme155@gmail.com>
2018-08-18 13:56:29 +0300

    Add key_error helper and Refactor key_error responses

--- app/views/model_view.py
+++ app/views/model_view.py
@@ -14,6 +14,19 @@ from app.response_helpers import convert_user_answers_list_to_json
 qtn_bp = Blueprint('question', __name__, url_prefix='/api/v1')
 
 
+# Helper function
+def check_for_key_error(resp):
+    """
+    function used to return a custom response
+     in case an out of index error is triggered.
+
+    Arguments:
+        resp -- object returned from the request
+    """
+    if isinstance(resp, KeyError):
+            return 'Question ' + str(resp) + ' does not exist'
+
+
 class QuestionsView(MethodView):
 
     methods = ['POST', 'GET']
@@ -67,9 +80,9 @@ class QuestionView(MethodView):
             return response('request must be of type json', 'failed', 400)
 
         resp = question_manager.get_question(qtn_id)
-        if isinstance(resp, KeyError):
-            return response('Question ' + str(resp) + ' does not exist',
-                            'failed', 400)
+        message = check_for_key_error(resp)
+        if message:
+            return response(message, 'failed', 400)
         return response_to_fetch_single_question(resp.jsonify(), 200)
 
     # PATCH
@@ -85,14 +98,15 @@ class QuestionView(MethodView):
             return response('request must be of type json', 'failed', 400)
 
         res = question_manager.get_question(qtn_id)
-        if isinstance(res, KeyError):
-            return response('Question ' + str(res) + ' does not exist',
-                            'failed', 400)
+        message = check_for_key_error(res)
+        if message:
+            return response(message, 'failed', 400)
 
         sent_data = request.get_json()
         title = sent_data.get('title')
         body = sent_data.get('body')
         tag = sent_data.get('tag')
+
         if title:
             res.title = title
         if body:
@@ -110,10 +124,11 @@ class QuestionView(MethodView):
         """
         if not request.content_type == 'application/json':
             return response('request must be of type json', 'failed', 400)
+
         respo = question_manager.delete_question(qtn_id)
-        if isinstance(respo, KeyError):
-            return response('Question ' + str(respo) + ' does not exist',
-                            'failed', 400)
+        message = check_for_key_error(respo)
+        if message:
+            return response(message, 'failed', 400)
         return response(
             'Question {0} has been deleted'.format(qtn_id), 'success', 200)
 
@@ -132,13 +147,14 @@ class AnswerView(MethodView):
         """
         if not request.content_type == 'application/json':
             return response('request must be of type json', 'failed', 400)
+
         sent_data = request.get_json()
         body = sent_data.get('body')
         respon = question_manager.get_question(qtn_id)
+        message = check_for_key_error(respon)
+        if message:
+            return response(message, 'failed', 400)
 
-        if isinstance(respon, KeyError):
-            return response('Question ' + str(respon) + ' does not exist',
-                            'failed', 400)
         answer = Answer(respon.id, body=body)
         respon.answers.append(answer.make_json())
         return response_for_returning_single_question(respon, 201)
@@ -155,9 +171,10 @@ class AnswerView(MethodView):
             return response('request must be of type json', 'failed', 400)
 
         respons = question_manager.get_question(qtn_id)
-        if isinstance(respons, KeyError):
-            return response('Question ' + str(respons) + ' does not exist',
-                            'failed', 400)
+        message = check_for_key_error(respons)
+        if message:
+            return response(message, 'failed', 400)
+
         answers = convert_user_answers_list_to_json(respons.answers)
         return response_for_get_all_answers(answers, 200)
 
