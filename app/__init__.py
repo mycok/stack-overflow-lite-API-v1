d9a2b53150c3ddfaa67c37dbab0a520397aed1a3
Michael Kibuuka <awesomeme155@gmail.com>
2018-08-23 23:50:07 +0300

    Transfer app from __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,1 @@
-from flask import Flask
-from flasgger import Swagger
-from app.views.model_view import qtn_bp
-from app.config import DevelopmentConfig
 
-
-app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
-app.register_blueprint(qtn_bp)
-swagger = Swagger(app)
