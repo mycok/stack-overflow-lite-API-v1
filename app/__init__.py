9b0e353448495a1e793225a5fc5169eff80368e0
Michael Kibuuka <awesomeme155@gmail.com>
2018-08-23 08:12:05 +0300

    Remove app factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,10 @@
 from flask import Flask
-from app.views.model_view import qtn_bp
 from flasgger import Swagger
+from app.views.model_view import qtn_bp
 from app.config import DevelopmentConfig
 
 
-def create_app(config_object=None):
-    app = Flask(__name__)
-    if config_object is not None:
-        app.config.from_object(config_object)
-    app.config.from_object(DevelopmentConfig)
-    app.register_blueprint(qtn_bp)
-    swagger = Swagger(app)
-    return app
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+app.register_blueprint(qtn_bp)
+swagger = Swagger(app)
