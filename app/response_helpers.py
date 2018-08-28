5ed26e5e70e14ecb552ff1778a044cfdfc640be1
Michael Kibuuka <awesomeme155@gmail.com>
2018-08-28 14:47:06 +0300

    Refactor response_helpers

--- app/response_helpers.py
+++ app/response_helpers.py
@@ -1,67 +1,68 @@
-from flask import make_response, jsonify
+# from flask import make_response, jsonify
 
 
-def response(message, status, status_code):
-    return make_response(jsonify({
-        "message": message,
-        "status": status
-    })), status_code
+# def response(message, status, status_code):
+#     return make_response(jsonify({
+#         "message": message,
+#         "status": status
+#     })), status_code
 
 
-def response_for_returning_single_question(question, status_code):
-    return make_response(jsonify({
-        'status': 'success',
-        'id': question.id,
-        'title': question.title,
-        'body': question.body,
-        'tag': question.tag,
-        'date_posted': question.posted_date,
-        'answers': question.answers
-    })), status_code
+# def response_for_returning_single_question(question, status_code):
+#     return make_response(jsonify({
+#         'status': 'success',
+#         'author': question.author,
+#         'id': question.id,
+#         'title': question.title,
+#         'body': question.body,
+#         'tag': question.tag,
+#         'date_posted': question.posted_date,
+#         'answers': question.answers
+#     })), status_code
 
 
-def response_for_get_all_questions(questions, status_code):
-    return make_response(jsonify({
-        'status': 'success',
-        'questions': questions
-    })), status_code
+# def response_for_get_all_questions(questions, status_code):
+#     return make_response(jsonify({
+#         'status': 'success',
+#         'questions': questions
+#     })), status_code
 
 
-def response_to_fetch_single_question(question, status_code):
-    return make_response(jsonify({
-        'status': 'success',
-        'question': question
-    })), status_code
+# def response_to_fetch_single_question(question, status_code):
+#     return make_response(jsonify({
+#         'status': 'success',
+#         'question': question
+#     })), status_code
 
 
-def response_for_get_all_answers(answers, status_code):
-    return make_response(jsonify({
-        'status': 'success',
-        'answers': answers
-    })), status_code
+# def response_for_get_all_answers(answers, status_code):
+#     return make_response(jsonify({
+#         'status': 'success',
+#         'answers': answers
+#     })), status_code
 
 
-def convert_user_answers_list_to_json(answers):
-    """
-    convert answers list to json
-    Arguments:
-        lst -- list of answers
-    """
+# def convert_user_answers_list_to_json(answers):
+#     """
+#     convert answers list to json
+#     Arguments:
+#         lst -- list of answers
+#     """
 
-    lst = []
-    for answer in answers:
-        lst.append(answer)
-    return lst
+#     lst = []
+#     for answer in answers:
+#         lst.append(answer)
+#     return lst
 
 
-def convert_list_to_json(lsty):
-    """
-    converts a list to json
-    Arguments:
-        lst -- list of objects
-    """
+# def convert_list_to_json(lsty):
+#     """
+#     converts a list to json
+#     Arguments:
+#         lst -- list of objects
+#     """
 
-    lst = []
-    for l in lsty:
-        lst.append(l.jsonify())
-    return lst
+#     lst = []
+#     for l in lsty:
+#         lst.append(l.jsonify())
+#     return lst
